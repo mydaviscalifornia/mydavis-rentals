48a55659c61848b017bce0fc557cbc5477611bd2
mydaviscalifornia <brandonmckee@gmail.com>
2026-02-28 08:50:43 -0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import DavisRentals from "./DavisRentals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<DavisRentals />);
